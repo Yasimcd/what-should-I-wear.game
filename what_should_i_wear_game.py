df58ee360e6e42570f3cac4f192b9539d41a2aa3
Yasim <91527193+Yasimcd@users.noreply.github.com>
2022-12-18 18:40:13 -0800

    Add files via upload

---
+++ what_should_i_wear_game.py
@@ -0,0 +1,7 @@
+print ("What shall I wear today?")
+firstname = input ("Please Enter Your First Name: ")
+temp = float(input ("What is Today's Temperature: "))
+if temp >= 70 :
+  print ("Hi " + firstname + " , It will be a warm day , T-shirt time!")
+else:
+  print("Hi " + firstname + ", You should probably bring a sweater")
